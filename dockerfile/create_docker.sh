7431c062ff8e47aa4a7f0b152f42c7170cc01a7d
Jakub Scholz <jakub@scholz.cz>
2017-01-30 10:40:53 +0100

    Update the docker hub deployment

--- dockerfile/create_docker.sh
+++ dockerfile/create_docker.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/env bash
 
-DOCKER_ORG_NAME=dbg
+DOCKER_ORG_NAME=scholzj
 DOCKER_IMAGE_NAME=dave-ui
-CIRCLE_SHA1=1.0.0
 
 # Get current location and dave-ui root
 WHEREAMI=`dirname "${0}"`
@@ -13,12 +12,12 @@ fi
 # Copy the DAVe binaries
 cp -r ${DAVE_UI_ROOT}/dist ${DAVE_UI_ROOT}/dockerfile
 
-# docker login -e="$DOCKER_EMAIL" -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD"
+docker login -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD"
 docker build -t ${DOCKER_ORG_NAME}/${DOCKER_IMAGE_NAME}:${CIRCLE_SHA1} ${DAVE_UI_ROOT}/dockerfile/
 docker tag -f ${DOCKER_ORG_NAME}/${DOCKER_IMAGE_NAME}:${CIRCLE_SHA1} docker.io/${DOCKER_ORG_NAME}/${DOCKER_IMAGE_NAME}:${CIRCLE_SHA1}
-#docker push ${DOCKER_ORG_NAME}/${DOCKER_IMAGE_NAME}:${CIRCLE_SHA1}
-#docker tag -f ${DOCKER_ORG_NAME}/${DOCKER_IMAGE_NAME}:${CIRCLE_SHA1} docker.io/${DOCKER_ORG_NAME}/${DOCKER_IMAGE_NAME}:${CIRCLE_BRANCH}
-#docker push ${DOCKER_ORG_NAME}/${DOCKER_IMAGE_NAME}:${CIRCLE_BRANCH}
+docker push ${DOCKER_ORG_NAME}/${DOCKER_IMAGE_NAME}:${CIRCLE_SHA1}
+docker tag -f ${DOCKER_ORG_NAME}/${DOCKER_IMAGE_NAME}:${CIRCLE_SHA1} docker.io/${DOCKER_ORG_NAME}/${DOCKER_IMAGE_NAME}:${CIRCLE_BRANCH}
+docker push ${DOCKER_ORG_NAME}/${DOCKER_IMAGE_NAME}:${CIRCLE_BRANCH}
 
 # Clean-up
 rm -rf ${DAVE_UI_ROOT}/dockerfile/dist
