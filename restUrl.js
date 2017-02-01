5f19294a62ecdafc1d88fe1826875d723cc30a91
Adam Netočný <adam.netocny.ext@deutsche-boerse.com>
2017-02-01 23:51:51 +0100

    Align the ttsave BE URL with the new one.

--- restUrl.js
+++ restUrl.js
@@ -1,3 +1,3 @@
-// window.baseRestURL = 'https://ttsave.snapshot.dave.dbg-devops.com/api/v1.0'; // 'http(s)://someUrl:port/path'
+// window.baseRestURL = 'https://api.ttsave.snapshot.dave.dbg-devops.com/api/v1.0'; // 'http(s)://someUrl:port/path'
 // window.baseRestURL = 'https://demo.dave.dbg-devops.com/api/v1.0'; // 'http(s)://someUrl:port/path'
 window.baseRestURL = '/api/v1.0'; // 'http(s)://someUrl:port/path'
