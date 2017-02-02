b3e48cd42cb607e051b6a286da186a745bda1906
Jakub Scholz <jakub@scholz.cz>
2017-02-02 18:50:26 +0100

    Fix wrong user selected into kubectl context

--- deployment/kube_deploy.sh
+++ deployment/kube_deploy.sh
@@ -6,6 +6,6 @@ chmod +x kubectl
 ./kubectl config set-cluster dave --insecure-skip-tls-verify=true --server=${KUBE_API}
 ./kubectl config set-credentials dave --username ${KUBE_USERNAME} --password ${KUBE_PASSWORD}
 #./kubectl config set-credentials ${KUBE_USERNAME} --token ${KUBE_PASSWORD}
-./kubectl config set-context default-context --cluster=dave --user=${KUBE_USERNAME}
+./kubectl config set-context default-context --cluster=dave --user=daves
 ./kubectl config use-context default-context
 ./kubectl set image deployment/dave-ui dave-ui=scholzj/dave-ui:${CIRCLE_SHA1} --namespace=${CIRCLE_BRANCH}
