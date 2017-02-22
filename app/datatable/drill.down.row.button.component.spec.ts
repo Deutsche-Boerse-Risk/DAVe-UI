b0a90137f58c5532b396f8fa4166f5b3a4e00f66
Adam Netočný <adam.netocny.ext@deutsche-boerse.com>
2017-02-22 12:50:19 +0100

    #3: Tests for table buttons.

---
+++ app/datatable/drill.down.row.button.component.spec.ts
@@ -0,0 +1,47 @@
+import {DebugElement} from "@angular/core";
+import {By} from "@angular/platform-browser";
+
+import {ComponentFixture, TestBed, async, ComponentFixtureAutoDetect, fakeAsync} from "@angular/core/testing";
+
+import {click} from "../../testing/index";
+import {RouterLinkStubDirective} from "../../testing/router.link.stub";
+
+import {DrillDownRowButtonComponent} from "./drill.down.row.button.component";
+
+describe('DrillDownRowButtonComponent', () => {
+
+    let comp: DrillDownRowButtonComponent;
+    let fixture: ComponentFixture<DrillDownRowButtonComponent>;
+    let de: DebugElement;
+
+    beforeEach(async(() => {
+        TestBed.configureTestingModule({
+            declarations: [DrillDownRowButtonComponent, RouterLinkStubDirective],
+            providers: [
+                {provide: ComponentFixtureAutoDetect, useValue: true}
+            ]
+        }).compileComponents();
+    }));
+
+    beforeEach(() => {
+        fixture = TestBed.createComponent(DrillDownRowButtonComponent);
+
+        comp = fixture.componentInstance;
+
+        // query for the title by CSS element selector
+        de = fixture.debugElement.query(By.directive(RouterLinkStubDirective));
+    });
+
+    it('navigates correctly', fakeAsync(() => {
+        comp.routerLink = ['/test', 'url'];
+        fixture.detectChanges();
+
+        let routerLink: RouterLinkStubDirective = de.injector.get(RouterLinkStubDirective);
+        let navigateSpy = spyOn(routerLink, 'onClick').and.callThrough();
+
+        click(de);
+
+        expect(navigateSpy).toHaveBeenCalled();
+        expect(routerLink.navigatedTo).toEqual(['/test', 'url']);
+    }));
+});
