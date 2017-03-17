1595e9c26f3be0c815342bdfdaf4e85dc52b4c19
Adam Netočný <adam.netocny.ext@deutsche-boerse.com>
2017-03-17 14:41:47 +0100

    Upgraded to 4.0.0-rc.4

--- restUrl.js
+++ restUrl.js
@@ -1,3 +1,3 @@
-// window.baseRestURL = 'https://api.master.snapshot.dave.dbg-devops.com'; // 'http(s)://someUrl:port/path'
+window.baseRestURL = 'https://api.master.snapshot.dave.dbg-devops.com'; // 'http(s)://someUrl:port/path'
 // window.baseRestURL = 'https://demo.dave.dbg-devops.com'; // 'http(s)://someUrl:port/path'
-window.baseRestURL = ''; // 'http(s)://someUrl:port/path'
+// window.baseRestURL = ''; // 'http(s)://someUrl:port/path'
