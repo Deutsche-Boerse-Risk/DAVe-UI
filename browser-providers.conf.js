06ce56e3147319df25967876d5a7385290d5b7e9
Adam Netočný <adam.netocny.ext@deutsche-boerse.com>
2017-03-27 16:33:22 +0200

    Added Chrome/FireFox latest into BrowserStack config.

--- browser-providers.conf.js
+++ browser-providers.conf.js
@@ -4,20 +4,39 @@ var localBrowsers = {
     IE: 'IE'
 };
 var customLaunchers = {
-    BS_CHROME: {
+    BS_CHROME_LATEST: {
+        base: 'BrowserStack',
+        browser: 'Chrome',
+        os: 'Windows',
+        os_version: '10'
+    },
+    BS_CHROME50: {
         base: 'BrowserStack',
         browser: 'Chrome',
         browser_version: '50.0',
         os: 'Windows',
         os_version: '10'
     },
-    BS_FIREFOX: {
+    BS_FIREFOX_LATEST: {
+        base: 'BrowserStack',
+        browser: 'Firefox',
+        os: 'Windows',
+        os_version: '10'
+    },
+    BS_FIREFOX50: {
         base: 'BrowserStack',
         browser: 'Firefox',
         browser_version: '50.0',
         os: 'Windows',
         os_version: '10'
     },
+    // BS_IE9: {
+    //     base: 'BrowserStack',
+    //     browser: 'IE',
+    //     browser_version: '9.0',
+    //     os: 'Windows',
+    //     os_version: '7'
+    // },
     BS_IE10: {
         base: 'BrowserStack',
         browser: 'IE',
@@ -46,6 +65,13 @@ var customLaunchers = {
         os: 'Windows',
         os_version: '10'
     },
+    BS_SAFARI7: {
+        base: 'BrowserStack',
+        browser: 'Safari',
+        browser_version: '7.1',
+        os: 'OS X',
+        os_version: 'Mavericks'
+    },
     BS_SAFARI8: {
         base: 'BrowserStack',
         browser: 'Safari',
@@ -73,11 +99,11 @@ var BrowserStack = {
     ALL: Object.keys(customLaunchers).filter(function (item) {
         return customLaunchers[item].base === 'BrowserStack';
     }),
-    CHROME: ['BS_CHROME'],
-    FIREFOX: ['BS_FIREFOX'],
-    IE: ['BS_IE10', 'BS_IE11'],
+    CHROME: ['BS_CHROME50', 'BS_CHROME_LATEST'],
+    FIREFOX: ['BS_FIREFOX50', 'BS_FIREFOX_LATEST'],
+    IE: [/*'BS_IE9', */'BS_IE10', 'BS_IE11'],
     EDGE: ['BS_EDGE13', 'BS_EDGE14'],
-    SAFARI: ['BS_SAFARI8', 'BS_SAFARI9', 'BS_SAFARI10']
+    SAFARI: ['BS_SAFARI7', 'BS_SAFARI8', 'BS_SAFARI9', 'BS_SAFARI10']
 };
 
 var LOCAL = {
