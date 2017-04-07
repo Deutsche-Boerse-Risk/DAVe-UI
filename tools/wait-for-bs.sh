123d3f3c2252779f717395cecce3774dacff5c47
Adam Netočný <adam.netocny.ext@deutsche-boerse.com>
2017-04-07 15:16:49 +0200

    Added random sleep timeout for BrowserStack lock  Please enter the commit message for your changes. Lines starting

--- tools/wait-for-bs.sh
+++ tools/wait-for-bs.sh
@@ -2,6 +2,7 @@
 
 while [ "$(grunt browserstack_status | grep 'Running sessions' | awk '{print substr($0,length,1)}')" != "0" ]
  do
-    echo "Waiting another 60 seconds for BrowserStack session..."
-    sleep 60
+    SLEEP_TIMEOUT=`shuf -i 30-150 -n 1`
+    echo "Waiting another $SLEEP_TIMEOUT seconds for BrowserStack session..."
+    sleep $SLEEP_TIMEOUT
  done
