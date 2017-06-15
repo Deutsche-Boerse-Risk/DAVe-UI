9feef45469f642901ff1b12c92a813c588014f14
Adam Netočný <adam.netocny.ext@deutsche-boerse.com>
2017-06-15 17:35:42 +0200

    Enable coveralls

--- gruntfile.js
+++ gruntfile.js
@@ -3,6 +3,18 @@ module.exports = function (grunt) {
 
     require('@dbg-riskit/dave-ui-common/tools/grunt/app.grunt.config')(grunt, 'dave.js');
 
+    grunt.config('coveralls', {
+        options: {
+            force: true
+        },
+        report: {
+            src: 'coverage/lcov.info'
+        }
+    });
+
+
+    grunt.loadNpmTasks('grunt-coveralls');
+
     // Build tasks
     grunt.registerTask('clean', ['cleanup:all']);
     grunt.registerTask('compile', ['sass', 'ts:compile']);
@@ -12,4 +24,10 @@ module.exports = function (grunt) {
     // Dist tasks
     grunt.registerTask('dist', ['cleanup:all', 'sass', 'cssmin', 'ngc', 'copy', 'rollup', 'cleanup:postDist']);
     grunt.registerTask('dist-run', ['dist', 'browserSync:dist']);
+
+
+    // CircleCI test tasks
+    grunt.registerTask('testCircleCI', ['testBrowserStack', 'coveralls']);
+    grunt.registerTask('testCircleCIMinimal', ['testBrowserStackMinimal', 'coveralls']);
+    grunt.registerTask('testCircleCIChrome', ['testBrowserStackChrome', 'coveralls']);
 };
